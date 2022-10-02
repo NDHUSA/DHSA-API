aa68deba8adbc1f62f4b3c9f67b6f8c056965485
yc97463 <hi@imych.one>
2022-10-02 10:33:38 +0800

    feat(gas/get-store): fetch spreadsheet data on GAS

---
+++ google-apps-script/get-store.js
@@ -0,0 +1,21 @@
+function doGet(request){
+  const targetSpreadSheet = SpreadsheetApp.openById("1j6_sbGFEdT00ldc3Aie-4B5ynwrpbtHhMcFnCnqMIq4");
+  const sheetName = targetSpreadSheet.getSheetByName('sheet1');
+  let sheetValues = sheetName.getDataRange().getValues();
+  
+  let jsonFormated = [];
+  skipId = 0;
+  sheetValues.forEach((data) => {
+    if(skipId==0){skipId+=1;return;};
+    if(data[1]==""&&data[2]==""){return;};
+    jsonFormated.push({
+      ['id']:data[0].split('-')[1],
+      ['name-zh']:data[1].toString(),
+      ['name-en']:data[2].toString(),
+    })
+  })
+  let exportFormat = JSON.stringify(jsonFormated);
+    let final = ContentService.createTextOutput(exportFormat).setMimeType(ContentService.MimeType.JSON);
+    Logger.log(final);
+    return final;
+}
