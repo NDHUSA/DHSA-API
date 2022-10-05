d0e71791c4cd87fd79cfe22780f597b4c3efd4e3
yc97463 <hi@imych.one>
2022-10-05 22:32:57 +0800

    fix(index): express host

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ app.get('/store', async (req, res) => {
   
 })
 
-app.listen(PORT, () => {
-  // console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(parseInt(process.env.PORT) || 8080, () => {
+  console.log(`Running on ${PORT}`);
 });
