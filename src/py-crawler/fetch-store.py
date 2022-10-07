730470730e1cb7a985cd546c9cc3ce5ea68cd45f
yc97463 <hi@imych.one>
2022-10-07 10:48:26 +0800

    fix(gas/store): update sheet name

--- src/py-crawler/fetch-store.py
+++ src/py-crawler/fetch-store.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'https://script.google.com/macros/s/AKfycbxwfVUAaUmpj2Sh7ldxGknQwSHlGETEfUS0GvCgPfeTJ2g4IMLFz4DLfHnQqN5C0TNkVg/exec'
+url = 'https://script.google.com/macros/s/AKfycbydJ9ELChSr5QL6CNCrI9g3N3RqH8lV2-ie-GLQfrE7U_qx1HifIEF07O1Del2NFNqavw/exec'
 resp = requests.get(url)
 
 print(resp.text)
