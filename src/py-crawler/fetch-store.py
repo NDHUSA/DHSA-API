0364026855ff76b934a8cff2126e28d870a13377
yc97463 <hi@imych.one>
2022-10-07 21:44:46 +0800

    feat(py/store):improve thumb resource host

--- src/py-crawler/fetch-store.py
+++ src/py-crawler/fetch-store.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'https://script.google.com/macros/s/AKfycby8dyw15iic-IXwqGwrQLXcWU-VFNilJz5Qrw4lNQ62vPsdeyOiiL7ei6gEw-PqZ-RcvA/exec'
+url = 'https://script.google.com/macros/s/AKfycbxGzZWP_KXGruKOJKlnBwv39n9C1P0tGeTyZLvH-WaghTU6mGWiQF9I6md15e9j3Ru6CA/exec'
 resp = requests.get(url)
 
 print(resp.text)
