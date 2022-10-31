22e8ee53bbe9e538e21975e72738908cc4d08d7d
yc97463 <hi@imych.one>
2022-10-31 16:23:20 +0800

    feat(src/gas/issue): add gas to create github issues from google form

---
+++ src/google-apps-script/create-ghRepo-issue.js
@@ -0,0 +1,55 @@
+var apiAuthToken = "";
+
+function onFormSubmit(e) {
+  var form = FormApp.getActiveForm();
+  var allResponses = form.getResponses();
+  var latestResponse = allResponses[allResponses.length - 1];
+  var response = latestResponse.getItemResponses();
+  var payload = {};
+  for (var i = 0; i < response.length; i++) {
+    var question = response[i].getItem().getTitle();
+    if (question == "請描述問題及事情經過") {
+      payload[1] = response[i].getResponse();
+    } else if (question == "可附上截圖、影片等說明資料") {
+      payload[2] = response[i].getResponse();
+    } else if (question == "我們該如何稱呼您？") {
+      payload[3] = response[i].getResponse();
+    } else if (question == "向您聯繫的 Email") {
+      payload[4] = response[i].getResponse();
+    } else if (question == "發生問題的路徑（請勿更動）") {
+      payload[5] = response[i].getResponse();
+    } else {
+    }
+  }
+
+  var issueTitle = "User Report Submit At " + latestResponse.getTimestamp();
+  var issueBody =
+    "| TimeStamp | Description | Attachment | Path |\n" +
+    "|---|---|---|---|\n" +
+    "| " +
+    latestResponse.getTimestamp() +
+    " | " +
+    payload[1] +
+    " | " +
+    (payload[2] ? "https://drive.google.com/open?id=" + payload[2] : "false") +
+    " | " +
+    payload[5] +
+    " |";
+
+  var response = UrlFetchApp.fetch(
+    "https://api.dhsa.dstw.dev/workflow/github/issue",
+    {
+      method: "POST",
+      contentType: "application/json",
+      payload: JSON.stringify({
+        title: issueTitle,
+        content: issueBody,
+        repo: "yc97463/DHSA-API",
+        labels: ["user report"],
+      }),
+      headers: {
+        Authorization: apiAuthToken,
+      },
+    }
+  );
+}
