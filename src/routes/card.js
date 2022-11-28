341caddda399c003fb578e99d2abde33bc70aad4
yc97463 <youualan87@gmail.com>
2022-11-28 12:50:45 +0800

    feat(r/card): add try catch for get membership

--- src/routes/card.js
+++ src/routes/card.js
@@ -10,20 +10,25 @@ app.get("/store", async (req, res) => {
 
 app.get("/membership/:uid", async (req, res) => {
   const { uid } = req.params;
-  const response = await fetch(process.env.CACHE + "/paying-member.json").then(
-    (response) => response.json()
-  );
-  let isMember = Boolean(false);
-  for (let i = 0; i < response.length; i++) {
-    if (uid == response[i].stuId) {
-      isMember = Boolean(true);
-      break;
+  try {
+    const response = await fetch(
+      process.env.CACHE + "/paying-member.json"
+    ).then((response) => response.json());
+    let isMember = Boolean(false);
+    for (let i = 0; i < response.length; i++) {
+      if (uid == response[i].stuId) {
+        isMember = Boolean(true);
+        break;
+      }
     }
-  }
-  if (isMember == Boolean(true)) {
-    res.status(200).json({ uid: uid, membership: "會費會員" });
-  } else {
-    res.status(200).json({ uid: uid, membership: "一般會員" });
+
+    if (isMember == Boolean(true)) {
+      res.status(200).json({ uid: uid, membership: "會費會員" });
+    } else {
+      res.status(200).json({ uid: uid, membership: "一般會員" });
+    }
+  } catch (err) {
+    res.status(500).json({ status: "fetch err" });
   }
 });
 
