40487d49cc9444021ac7b43b4c7a19e66fb8a9b3
yc97463 <hi@imych.one>
2023-03-30 00:04:10 +0800

    feat(r/announce): Handle exception errors 1. pid only in Int type 2. HTTP 404

--- src/routes/announce.js
+++ src/routes/announce.js
@@ -1,15 +1,30 @@
 import express from "express";
 import pkg from "rss-to-json";
+import fetch from "node-fetch";
 
 const app = express.Router();
 
 app.get("/:pid", async (req, res) => {
   const { parse } = pkg;
-  const pid = req.params.pid;
-  let response = await parse(
-    `https://dhsa.ndhu.edu.tw/p/427-1110-${pid}.php?Lang=zh-tw`
-  );
-  res.status(200).json(response);
+  const pid = parseInt(req.params.pid);
+  const rpageEndpoint = `https://dhsa.ndhu.edu.tw`;
+  const checkHTTPStatus = await fetch(
+    `${rpageEndpoint}/p/427-1110-${pid}.php?Lang=zh-tw`,
+    {
+      method: "GET",
+    }
+  ).then((x) => x.status);
+
+  if (checkHTTPStatus != 200) {
+    res
+      .status(checkHTTPStatus)
+      .json({ status: false, msg: "Something error." });
+  } else {
+    let response = await parse(
+      `${rpageEndpoint}/p/427-1110-${pid}.php?Lang=zh-tw`
+    );
+    res.status(200).json(response);
+  }
 });
 
 export default app;
