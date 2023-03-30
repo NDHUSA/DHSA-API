34e4a9b927c5225f1f02bff07d68016e7a1a0134
yc97463 <hi@imych.one>
2023-03-31 00:59:21 +0800

    feat(r/room): add open door API

--- src/routes/room.js
+++ src/routes/room.js
@@ -2,4 +2,19 @@ import express from "express";
 
 const app = express.Router();
 
+app.get("/:roomId/open", async (req, res) => {
+  const { roomId } = req.params;
+  const { token } = req.headers;
+  function exp(days) {
+    return days * 24 * 60 * 60; // 1 Days
+  }
+  const expired_timestamp = Math.floor(Date.now() / 1000) + exp(30 * 5); // n Days;
+  res.status(200).json({
+    status: true,
+    roomId: roomId,
+    access_expired: expired_timestamp,
+    msg: `The door ${roomId} has been opened.\nIt will expired at ${expired_timestamp}.`,
+  });
+});
+
 export default app;
