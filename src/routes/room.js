ac26f321d3a9aa70a7d1ea78927bbfbce389e985
yc97463 <hi@imych.one>
2023-03-31 22:48:02 +0800

    feat(r/room): add room API to create, add, and update rooms

--- src/routes/room.js
+++ src/routes/room.js
@@ -1,33 +1,87 @@
 import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
 
 const app = express.Router();
 
+dotenv.config();
+const uri = "mongodb://" + process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.get("/", async (req, res) => {
-  res.status(200).json([
-    { id: 1, name: "Room 1" },
-    { id: 2, name: "Room 2" },
-  ]);
+  await client.connect();
+  const database = client.db("dhsa-service");
+  const collection = database.collection("room_list");
+  const cursor = collection.find({});
+  const result = await cursor.toArray();
+  console.log(result);
+  res.status(200).json(result);
 });
 
-app.get("/:roomId/open", async (req, res) => {
-  const { roomId } = req.params;
+app.post("/", async (req, res) => {
   const { token } = req.headers;
-  function exp(days) {
-    return days * 24 * 60 * 60; // 1 Days
+  const { name, description, color, enable } = req.body;
+  const timestamp = new Date();
+
+  await client.connect();
+  const database = client.db("dhsa-service");
+  const collection = database.collection("room_list");
+  const insertData = {
+    name: name,
+    description: description,
+    created_at: timestamp,
+    updated_at: timestamp,
+    color: color,
+    enable: enable,
+  };
+  try {
+    const result = await collection.insertOne(insertData);
+    console.log(result);
+    res.status(200).json({
+      status: true,
+      msg: `The room ${name} has been created.`,
+    });
+  } catch (err) {
+    console.log(err);
+    // res.status(409).json({ error: "The room is already exist." });
+    res.status(500).json({ err: "database connection error" });
   }
-  const expired_timestamp = Math.floor(Date.now() / 1000) + exp(30 * 5); // n Days;
-  res.status(200).json({
-    status: true,
-    roomId: roomId,
-    access_expired: expired_timestamp,
-    msg: `The door ${roomId} has been opened.\nIt will expired at ${expired_timestamp}.`,
-  });
 });
 
-app.get("/:roomId/reservation", async (req, res) => {
-  const { roomId } = req.params;
-  const { from, to } = req.query;
-  res.status(200).json([{}]);
+app.patch("/:room_id", async (req, res) => {
+  const { room_id } = req.params;
+  const { token } = req.headers;
+  const { name, description, color, enable } = req.body;
+  const timestamp = new Date();
+
+  await client.connect();
+  const database = client.db("dhsa-service");
+  const collection = database.collection("room_list");
+  const updateData = {
+    name: name,
+    description: description,
+    updated_at: timestamp,
+    color: color,
+    enable: enable,
+  };
+  try {
+    const result = await collection.updateOne(
+      { _id: new ObjectId(room_id) },
+      { $set: updateData }
+    );
+    console.log(result);
+    res.status(200).json({
+      status: true,
+      msg: `The room ${name} has been updated.`,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "database connection error" });
+  }
+  // res.status(404).json({ error: "The room is not found." });
 });
 
 export default app;
