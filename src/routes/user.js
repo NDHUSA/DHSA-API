4893075246625dda7d4dd64583f1fc08f41dde07
yc97463 <hi@imych.one>
2023-04-05 22:48:16 +0800

    refactor(r/user): remove /status

--- src/routes/user.js
+++ src/routes/user.js
@@ -11,59 +11,6 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-app.get("/status", async (req, res) => {
-  const { token } = req.headers;
-
-  const userInfo = await fetch(process.env.HOST + "/auth/token", {
-    headers: {
-      token: token,
-    },
-  }).then((x) => x.json());
-  if (!userInfo.status) {
-    res.status(401).json({ status: false, msg: "Invalid token." });
-    return;
-  }
-
-  await client.connect();
-  const database = client.db("dhsa-service");
-  const collection = database.collection("users");
-  const isMember =
-    (await collection.countDocuments({
-      email: userInfo.email,
-    })) === 1
-      ? true
-      : false;
-  if (isMember) {
-    res.status(200).json({ status: true, isMember: isMember });
-  } else {
-    const isNDHUer = await fetch(process.env.HOST + "/auth/ndhuLDAP", {
-      headers: { token: token },
-    }).then((x) => x.json());
-    const timestamp = new Date();
-    const insertData = {
-      created_at: timestamp,
-      updated_at: timestamp,
-      enabled: true,
-      email: userInfo.email,
-      name: userInfo.name,
-      avatar: userInfo.avatar,
-      note: null,
-      ndhu_role: isNDHUer.role || null,
-      sa_role: null,
-    };
-    try {
-      const result = await collection.insertOne(insertData);
-      res.status(200).json({
-        status: true,
-        msg: `The user ${userInfo.name} has been created.`,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ err: "database connection error" });
-    }
-  }
-});
-
 app.get("/me", async (req, res) => {
   const { token } = req.headers;
   const userInfo = await fetch(process.env.HOST + "/auth/token", {
@@ -79,14 +26,50 @@ app.get("/me", async (req, res) => {
   await client.connect();
   const database = client.db("dhsa-service");
   const collection = database.collection("users");
-  const result = await collection.findOne(
-    {
-      _id: new ObjectId(userInfo.user_oid),
-    },
-    { projection: { enabled: 0, note: 0, created_at: 0, updated_at: 0 } }
-  );
 
-  res.status(200).json(result);
+  const isMember =
+    (await collection.countDocuments({
+      email: userInfo.email,
+    })) === 1
+      ? true
+      : false;
+
+  if (isMember) {
+    const result = await collection.findOne(
+      {
+        _id: new ObjectId(userInfo.user_oid),
+      },
+      { projection: { enabled: 0, note: 0, created_at: 0, updated_at: 0 } }
+    );
+
+    res.status(200).json(result);
+  } else {
+    const ndhu_role = await fetch(process.env.HOST + "/auth/ndhuLDAP", {
+      headers: { token: token },
+    }).then((x) => x.json());
+    const timestamp = new Date();
+    const insertData = {
+      created_at: timestamp,
+      updated_at: timestamp,
+      enabled: true,
+      email: userInfo.email,
+      name: userInfo.name,
+      avatar: userInfo.avatar,
+      note: null,
+      ndhu_role: ndhu_role.role || null,
+      sa_role: null,
+    };
+    try {
+      const result = await collection.insertOne(insertData);
+      res.status(200).json({
+        status: true,
+        msg: `The user ${userInfo.name} has been created.`,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ err: "database connection error" });
+    }
+  }
 });
 
 app.patch("/ndhu-role", async (req, res) => {
