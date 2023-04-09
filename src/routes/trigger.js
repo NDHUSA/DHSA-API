3b4c6f2fea41c22269fa71b269aa59933460417c
yc97463 <hi@imych.one>
2023-04-09 18:10:29 +0800

    refactor(r/trigger): make an async function to doUpdate request

--- src/routes/trigger.js
+++ src/routes/trigger.js
@@ -12,12 +12,12 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-app.all("/card/store", async (req, res) => {
+async function doUpdate(table_name, req, res) {
   await client.connect();
   const database = client.db("dhsa-service");
   const collection = database.collection("static_data");
   const file_id = await collection
-    .findOne({ name: "partner_store" })
+    .findOne({ name: table_name })
     .then((x) => x.GAS_FILE_ID);
 
   try {
@@ -28,7 +28,7 @@ app.all("/card/store", async (req, res) => {
     const timestamp = new Date();
 
     await collection.updateOne(
-      { name: "partner_store" },
+      { name: table_name },
       { $set: { updated_at: timestamp, value: resp } }
     );
     await client.close();
@@ -36,42 +36,21 @@ app.all("/card/store", async (req, res) => {
       .status(200)
       .json({ status: true, msg: "update success", updated_at: timestamp });
   } catch (err) {
-    console.log(err);
     res
       .status(500)
       .json({ status: false, msg: "err when fetching or database" });
   }
+}
+
+app.patch("/card/store", async (req, res) => {
+  await doUpdate("partner_store", req, res);
 });
 
 app.patch("/card/membership", async (req, res) => {
-  await client.connect();
-  const database = client.db("dhsa-service");
-  const collection = database.collection("static_data");
-  const file_id = await collection
-    .findOne({ name: "has_paid_membership" })
-    .then((x) => x.GAS_FILE_ID);
+  await doUpdate("has_paid_membership", req, res);
+});
 
-  try {
-    const resp = await fetch(
-      `https://script.google.com/macros/s/${file_id}/exec`
-    ).then((x) => x.json());
 
-    const timestamp = new Date();
-
-    await collection.updateOne(
-      { name: "has_paid_membership" },
-      { $set: { updated_at: timestamp, value: resp } }
-    );
-    await client.close();
-    res
-      .status(200)
-      .json({ status: true, msg: "update success", updated_at: timestamp });
-  } catch (err) {
-    console.log(err);
-    res
-      .status(500)
-      .json({ status: false, msg: "err when fetching or database" });
-  }
 });
 
 export default app;
