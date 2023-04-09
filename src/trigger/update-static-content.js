070a0ea5ef1180e9aa8030d84da35d67b626356e
yc97463 <hi@imych.one>
2023-04-09 16:39:17 +0800

    feat(workflows/static-content): add scheduled poke update-static-content yaml

---
+++ src/trigger/update-static-content.js
@@ -0,0 +1,26 @@
+import fetch from "node-fetch";
+
+const uri = "https://preview.api.dhsa.dstw.dev/trigger";
+console.log("URI: " + uri);
+
+try {
+  console.log("start to poke partner_store");
+  const partner_store = await fetch(uri + "/card/store", {
+    method: "PATCH",
+  }).then((x) => x.json());
+  console.log("partner_store API reponse: " + partner_store);
+} catch (err) {
+  console.log("partner_store has some error: " + err);
+}
+
+try {
+  console.log("start to poke has_paid_membership");
+  const has_paid_membership = await fetch(uri + "/card/membership", {
+    method: "PATCH",
+  }).then((x) => x.json());
+  console.log("has_paid_membership API says: " + has_paid_membership);
+} catch (err) {
+  console.log("has_paid_membership has some error: " + err);
+}
+
+console.log("All done!");
