dbf3754e8fe233dd2ef33d4d423c61aa791d8089
yc97463 <hi@imych.one>
2023-11-10 16:33:52 +0800

    feat: route use /card/index

---
+++ src/routes/card/index.js
@@ -0,0 +1,72 @@
+import express from "express";
+import fetch from "node-fetch";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import member from "./member.js";
+
+const app = express.Router();
+
+dotenv.config();
+const uri = "mongodb://" + process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/store", async (req, res) => {
+  await client.connect();
+  const database = client.db("dhsa-service");
+  const collection = database.collection("static_data");
+  const partner_store = await collection.findOne({
+    name: "partner_store",
+  });
+  await client.close();
+  res
+    .status(200)
+    .json({ updated_at: partner_store.updated_at, value: partner_store.value });
+});
+
+app.get("/membership/:uid", async (req, res) => {
+  const uid = req.params.uid.toUpperCase();
+
+  await client.connect();
+  const database = client.db("dhsa-service");
+  const collection = database.collection("static_data");
+  const isMember = await collection.countDocuments({
+    name: "has_paid_membership",
+    value: { $elemMatch: { id: uid } },
+  });
+  await client.close();
+
+  try {
+    if (isMember) {
+      res.status(200).json({ status: true, uid: uid, membership: "會費會員" });
+    } else {
+      res.status(200).json({ status: true, uid: uid, membership: "一般會員" });
+    }
+  } catch (err) {
+    res.status(500).json({ status: false, msg: "fetch err" });
+  }
+});
+
+app.post("/membership/:uid", async (req, res) => {
+  const { uid } = req.params;
+  const { token } = req.headers;
+  if (token != process.env.membership_afterpay_token) {
+    res.status(401).json({ status: false, msg: "Auth error" });
+  } else {
+    const response = await fetch(
+      "https://docs.google.com/forms/d/e/1FAIpQLSdFTO7XQghlOXWkjaP1T6hajuzpR9eBCyitUJA2ZZiofgm_Bg/formResponse",
+      { method: "POST", body: new URLSearchParams({ "entry.305453546": uid }) }
+    );
+    if (response.status == 200) {
+      res.status(200).json({ status: true, uid: uid });
+    } else {
+      res
+        .status(response.status)
+        .json({ status: false, msg: "Form response error", uid: uid });
+    }
+  }
+});
+
+export default app;
---
+++ src/routes/card/member.js
@@ -0,0 +1,46 @@
+import express, { response } from "express";
+import fetch from "node-fetch";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+const app = express.Router();
+
+dotenv.config();
+const uri = "mongodb://" + process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Check User Status
+app.get("/all", async (req, res) => {
+  const { access_token } = req.headers;
+
+  // Auth Access Toekn
+  if (access_token != process.env.CARD_MEMBER_ACCESS_TOKEN) {
+    res.status(401).json({
+      status: false,
+      msg: "Access token invalid",
+    });
+    return;
+  }
+
+  try {
+    await client.connect();
+    const database = client.db("dhsa-service");
+    const collection = database.collection("users");
+    const query = await collection.find({}).toArray();
+    res.status(200).json(query);
+
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      error: err,
+      msg: "something wrong when connecting to database",
+    });
+  } finally {
+    await client.close();
+  }
+});
+
+export default app;
